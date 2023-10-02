98cbfc528262563fd79cec211ecacda80ee2511a
Gabriel Galeski <galeski35@gmail.com>
2023-10-02 13:37:18 -0300

    feat: implementa imc na page2

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -8,10 +8,40 @@ class Pagina2Page extends StatefulWidget {
 }
 
 class _Pagina2PageState extends State<Pagina2Page> {
+  TextEditingController peso = TextEditingController(text: "");
+  TextEditingController altura = TextEditingController(text: "");
+  var imc = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Column(
+      children: [
+        const SizedBox(
+          height: 100,
+        ),
+        TextField(
+          controller: peso,
+          decoration: const InputDecoration(
+            hintText: "Peso",
+          ),
+        ),
+        TextField(
+          controller: altura,
+          decoration: const InputDecoration(
+            hintText: "Altura",
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              imc = int.parse(peso.text) ~/ ((int.parse(altura.text) * 2) / 100);
+            });
+          },
+          child: const Text("Calcular"),
+        ),
+        Text(
+          "Seu IMC é: $imc",
+        ),
+      ],
     );
   }
 }
